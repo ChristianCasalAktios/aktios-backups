1fd56da4d74890877a25e82f99d9b96ac0e133b3
ChristianCasalAktios <65538901+ChristianCasalAktios@users.noreply.github.com>
2020-06-09 09:43:44 +0200

    Initial Commit

---
+++ src/main/java/com/aktiosbackups/aktiosbackups/AktiosBackupsApplication.java
@@ -0,0 +1,13 @@
+package com.aktiosbackups.aktiosbackups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AktiosBackupsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AktiosBackupsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aktiosbackups/aktiosbackups/AktiosBackupsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aktiosbackups.aktiosbackups;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AktiosBackupsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
